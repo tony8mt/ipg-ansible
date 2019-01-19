5c1e65f0f6851c8c748c5b68c5aecbcb1484f43d
Tony Thomas <tony8mt@gmail.com>
2019-01-20 08:05:35 +1100

    Formated

--- bashscript/thousands_separator.sh
+++ bashscript/thousands_separator.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "my arg :"  $1
 
 if  [[ ! $1 =~ ^[0-9]+$ ]] || [ $# -ne 1 ] ; then
     echo " Invalid argument"
     exit 1
 else    
+    echo "My argument :  $1 "
+    echo "Thousand Separated value : "
     echo $1 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'
 fi
